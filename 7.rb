70831f8ec193c41b58868767da5e76c0633826b1
Ryan Slade <ryanslade@gmail.com>
2009-09-17 20:28:39 +0100

    Added is_prime? method into helper

--- 7.rb
+++ 7.rb
@@ -1,32 +1,11 @@
-def is_prime?(n)
-  if (n == 1) 
-    return false
-	elsif (n < 4) 
-	  return true
-	elsif (n % 2 == 0) 
-	  return false
-	elsif (n < 9) 
-	  return true
-	elsif (n % 3 == 0) 
-	  return false
-	else
-		r = Math.sqrt(n)
-		f = 5;
-		while (f <= r) do
-			return false if (n % f == 0)
-			return false if (n % (f+2) == 0)
-			f=f+6
-		end
-		return true
-	end
-end
+require "helper"
 
 def primes(limit)
   yield 2
   count = 1
   i = 3
   loop do
-    if (is_prime?(i))
+    if (Helper.is_prime?(i))
       count += 1
       yield [i,count]
       break if count == limit
--- helper.rb
+++ helper.rb
@@ -5,4 +5,27 @@ class Helper
     return n * factorial(n-1)
   end
 
+  def self.is_prime?(n)
+    if (n == 1) 
+      return false
+  	elsif (n < 4) 
+  	  return true
+  	elsif (n % 2 == 0) 
+  	  return false
+  	elsif (n < 9) 
+  	  return true
+  	elsif (n % 3 == 0) 
+  	  return false
+  	else
+  		r = Math.sqrt(n)
+  		f = 5;
+  		while (f <= r) do
+  			return false if (n % f == 0)
+  			return false if (n % (f+2) == 0)
+  			f=f+6
+  		end
+  		return true
+  	end
+  end
+
 end
