5fc766803a903ad1c044bd6e84de6d30991dbc8b
Ryan Slade <ryanslade@gmail.com>
2009-09-17 23:43:03 +0100

    Pointless speedup

--- 19.rb
+++ 19.rb
@@ -2,8 +2,8 @@ require "date"
 
 d = Date.civil(1901,1,1)
 count = 0
-
-while d != Date.civil(2001,1,1) do
+ed = Date.civil(2001,1,1)
+while d != ed do
   count += 1 if (d.wday == 0 && d.mday == 1)
   d = d.next
 end
