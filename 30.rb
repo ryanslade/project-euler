779989a5f0d203e80d5a2fd0b78e7f5e67ad606b
Ryan Slade <ryanslade@gmail.com>
2009-09-19 12:01:12 +0100

    Worked out upper limit for problem 30

--- 30.rb
+++ 30.rb
@@ -2,7 +2,17 @@ def sum_of_powers(n, p)
   n.to_s.each_char.to_a.collect { |x| x.to_i**p }.inject(0) { |mem, var| mem += var }
 end
 
-# Not sure how to figure out when to stop
-numbers = (2..2000000).select { |n| sum_of_powers(n, 5) == n }
+max_per_digit = 9**5
+digits = 0
+done = false
+
+while (!done)
+  digits += 1
+  done = ("9"*digits).to_i > max_per_digit*digits
+end
+
+limit = ("9"*digits).to_i
+
+numbers = (2..limit).select { |n| sum_of_powers(n, 5) == n }
 
 puts numbers.inject(0) { |mem, var| mem += var }
