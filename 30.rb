b7a71b8c4f887feefec990887b4cff2a318c280c
Ryan Slade <ryanslade@gmail.com>
2009-09-19 11:52:50 +0100

    Pointless decrease in LOC

--- 30.rb
+++ 30.rb
@@ -2,12 +2,7 @@ def sum_of_powers(n, p)
   n.to_s.each_char.to_a.collect { |x| x.to_i**p }.inject(0) { |mem, var| mem += var }
 end
 
-numbers = []
-
 # Not sure how to figure out when to stop
-
-(2..2000000).each do |n|
-  numbers << n if sum_of_powers(n, 5) == n
-end
+numbers = (2..2000000).select { |n| sum_of_powers(n, 5) == n }
 
 puts numbers.inject(0) { |mem, var| mem += var }
