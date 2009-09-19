0b7a3cdffeb91e88ffe76309bb51ac8360d5ffdc
Ryan Slade <ryanslade@gmail.com>
2009-09-19 19:04:56 +0100

    Problem 36

---
+++ 36.rb
@@ -0,0 +1,11 @@
+def palindrome?(n, base=10)
+  n.to_s(base) == n.to_s(base).reverse
+end
+
+palindromes = []
+
+(1..1000000).each do |n|
+  palindromes << n if palindrome?(n) && palindrome?(n,2)
+end
+
+puts palindromes.inject(0) { |mem, var| mem += var }
