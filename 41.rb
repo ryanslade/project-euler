4813a0ee2cc779a81031a8c99197c1613ee1d777
Ryan Slade <ryanslade@gmail.com>
2009-09-20 18:43:10 +0100

    Problem 41

---
+++ 41.rb
@@ -0,0 +1,17 @@
+require "helper"
+
+largest = 0
+
+(9.downto(2)).each do |n|
+  numbers = (1..n).to_a.permutation.to_a.sort.reverse
+  numbers.each do |x|
+    t = x.inject("") { |mem, var| mem += var.to_s }.to_i
+    if Helper.prime?(t)
+      largest = t
+      break
+    end
+  end
+  break unless largest == 0
+end
+
+puts largest
