1be8f3adbb2a1f1a0f3e697c727ff78b651451cb
Ryan Slade <ryanslade@gmail.com>
2012-05-26 08:24:43 +0100

    Made 14.go a little more idiomatic (i think)

--- 14.go
+++ 14.go
@@ -1,33 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var longest uint = 0
-	var i uint = 1
+	var longest, i uint = 0, 1
 
-	for i < 1000000 {
+	for ; i < 1000000; i++ {
 		count := calculateChain(i)
 		if count > longest {
 			longest = count
 			fmt.Println(count)
 		}
-
-		i++
 	}
 }
 
 func calculateChain(n uint) uint {
-	var count uint = 0
-	for n != 1 {
+	var count uint = 1
+	for ; n != 1; count++ {
 		if (n % 2) == 0 {
 			n = n / 2
 		} else {
 			n = 3*n + 1
 		}
-		count++
 	}
-	return count + 1
+	return count
 }
